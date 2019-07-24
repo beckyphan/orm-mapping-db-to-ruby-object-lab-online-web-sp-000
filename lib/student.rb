9346ca85d339788a719a16072f36bc58b3c4051c
Becky Phan <beckyphan@live.com>
2019-07-24 01:34:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,9 +105,24 @@ class Student
   end
 
   def self.first_student_in_grade_10
+    sql = <<-SQL 
+      SELECT * 
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL 
+
+    DB[:conn].execute(sql)
   end
 
   def self.all_students_in_grade_X(grade)
+    sql = <<-SQL 
+      SELECT * 
+      FROM students
+      WHERE grade = ?
+    SQL 
+
+    DB[:conn].execute(sql)
   end
 
 end
